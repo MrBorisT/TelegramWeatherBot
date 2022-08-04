b6cab34c85f76d909302a6d6fc0a2ac731aec3e3
Boris <trofimov.boris.92@gmail.com>
2022-08-04 21:48:09 +0900

    Added translation to russian, using json

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ import (
 )
 
 type CallResults map[string]interface{}
-type Responses map[string]string
 
 var weatherApiToken string
+var responses map[string]string
 
 func main() {
 	err := godotenv.Load()
@@ -41,37 +41,45 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	lastCities := make(map[string]string)
-	languages := make(map[string]string)
+
+	responsesBytes, err := ioutil.ReadFile("./responses.json")
+	if err != nil {
+		log.Fatal("Error opening file: ", err)
+	}
+	responses = make(map[string]string)
+	if err = json.Unmarshal(responsesBytes, &responses); err != nil {
+		log.Fatal("Error during unmarshal: ", err)
+	}
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
+		language := update.Message.From.LanguageCode
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 		if update.Message.IsCommand() {
 			switch update.Message.Command() {
 			case "start":
-				msg.Text = "Enter the city!\nI'll get the info about the current weather up there!"
-				languages[update.Message.Chat.UserName] = update.Message.From.LanguageCode
+				msg.Text = GetText("start", language)
 			case "about":
 				msg.Text = "made by MrBorisT"
 			case "hello":
 				switch h := update.Message.Time().Hour(); {
 				case h >= 0 && h < 6:
-					msg.Text = "Good night, " + update.Message.Chat.LastName + "!"
+					msg.Text = GetText("good_night", language) + ", " + update.Message.Chat.LastName + "!"
 				case h >= 6 && h < 12:
-					msg.Text = "Good morning, " + update.Message.Chat.LastName + "!"
+					msg.Text = GetText("good_morning", language) + ", " + update.Message.Chat.LastName + "!"
 				case h >= 12 && h < 18:
-					msg.Text = "Good afternoon, " + update.Message.Chat.LastName + "! "
+					msg.Text = GetText("good_afternoon", language) + ", " + update.Message.Chat.LastName + "! "
 				default:
-					msg.Text = "Good evening, " + update.Message.Chat.LastName + "!"
+					msg.Text = GetText("good_evening", language) + ", " + update.Message.Chat.LastName + "!"
 				}
 			case "last_city":
 				if city, ok := lastCities[update.Message.Chat.UserName]; ok {
 					SetMsgCityWeather(city, update, &msg)
 				} else {
-					msg.Text = "It seems like you have not requested any cities, maybe try looking up one?"
+					msg.Text = GetText("last_city_fail", language)
 				}
 			}
 		} else {
@@ -115,7 +123,7 @@ func WeatherAPI(request string) CallResults {
 	return sr
 }
 
-func FormatWeather(cr CallResults) string {
+func FormatWeather(cr CallResults, language string) string {
 	if cr == nil {
 		return "Invalid request"
 	}
@@ -124,12 +132,12 @@ func FormatWeather(cr CallResults) string {
 	wind := cr["wind"].(map[string]interface{})
 	clouds := cr["clouds"].(map[string]interface{})
 
-	ans := "Temperature: " + fmt.Sprintf("%.1f", main["temp"].(float64)) + "°C\n"
-	ans += "Description: " + weather.(map[string]interface{})["description"].(string) + "\n"
-	ans += "Atmospheric Pressure: " + fmt.Sprintf("%.0f", main["pressure"].(float64)*0.75006157584566) + " mmHg\n" // convert hPa to mm Hg
-	ans += "Air Moisture: " + fmt.Sprintf("%.0f", main["humidity"].(float64)) + "%\n"
-	ans += "Wind Speed: " + fmt.Sprintf("%.0f", wind["speed"].(float64)) + "m/s\n"
-	ans += "Cloudiness: " + fmt.Sprintf("%.0f", clouds["all"].(float64)) + "%\n"
+	ans := GetText("temperature", language) + ": " + fmt.Sprintf("%.1f", main["temp"].(float64)) + "°C\n"
+	ans += GetText("description", language) + ": " + weather.(map[string]interface{})["description"].(string) + "\n"
+	ans += GetText("pressure", language) + ": " + fmt.Sprintf("%.0f", main["pressure"].(float64)*0.75006157584566) + " " + GetText("mmhg", language) + "\n" // convert hPa to mm Hg
+	ans += GetText("humidity", language) + ": " + fmt.Sprintf("%.0f", main["humidity"].(float64)) + "%\n"
+	ans += GetText("wind_speed", language) + ": " + fmt.Sprintf("%.0f", wind["speed"].(float64)) + GetText("mps", language) + "\n"
+	ans += GetText("cloudiness", language) + ": " + fmt.Sprintf("%.0f", clouds["all"].(float64)) + "%\n"
 
 	return ans
 }
@@ -137,6 +145,13 @@ func FormatWeather(cr CallResults) string {
 func SetMsgCityWeather(city string, update tgbotapi.Update, msg *tgbotapi.MessageConfig) {
 	apiAddress := getAPIAddress(city, weatherApiToken, update.Message.From.LanguageCode)
 	currentWeather := WeatherAPI(apiAddress)
-	msg.Text = FormatWeather(currentWeather)
+	msg.Text = FormatWeather(currentWeather, update.Message.From.LanguageCode)
 	msg.ReplyToMessageID = update.Message.MessageID
+}
+
+func GetText(id, lang string) string {
+	if lang == "ru" {
+		id += "_ru"
+	}
+	return responses[id]
 }
